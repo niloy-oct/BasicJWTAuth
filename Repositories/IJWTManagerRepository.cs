2bee433909881cabbcd07117c2e3c74936f9800e
Mahedi Hasan Niloy <niloy.oct@gmail.com>
2023-09-14 13:26:16 +0600

    add JWTManagerRepository class

---
+++ Repositories/IJWTManagerRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BasicJWTAuth.Models;
+
+namespace BasicJWTAuth.Repositories
+{
+    public interface IJWTManagerRepository
+    {
+        Tokens AuthenticateTokes(User user);
+    }
+}
---
+++ Repositories/JWTManagerRepository.cs
@@ -0,0 +1,25 @@
+﻿using BasicJWTAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicJWTAuth.Repositories
+{
+    public class JWTManagerRepository : IJWTManagerRepository
+    {
+        // make this static//
+        private Dictionary<string, string> userRecords = new Dictionary<string, string>
+        {
+            {"Niloy","HPLMIS"},
+            {"Admin","HPLMIS123"},
+            {"MIS","mis"},
+        };
+
+
+        public Tokens AuthenticateTokes(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
